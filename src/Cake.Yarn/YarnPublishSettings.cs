5b5f22fa6791ed070ece42a7d10f3e7916f69e26
Chris Munoz <cmunoz@extraspace.com>
2020-12-25 11:03:53 -0700

    Updated the publish class

--- src/Cake.Yarn/YarnPublishSettings.cs
+++ src/Cake.Yarn/YarnPublishSettings.cs
@@ -2,8 +2,23 @@
 {
     public class YarnPublishSettings : YarnRunnerSettings
     {
+        private string _newVersion;
+        private string _tag;
+
         public YarnPublishSettings() : base("publish")
         {
+        }
+
+        public YarnPublishSettings NewVersion(int major, int minor, int patch)
+        {
+            _newVersion = $"{major}.{minor}.{patch}";
+            return this;
+        }
+
+        public YarnPublishSettings Tag(string tag)
+        {
+            _tag = tag;
+            return this;
         }
     }
 }
